e3035122e9689086844fae44d090863723660439
Papa Kofi Boahen <humblepapa1234@gmail.com>
2024-07-30 11:57:21 +0000

    Update github links

--- lib/src/features/contact/social_info.dart
+++ lib/src/features/contact/social_info.dart
@@ -31,7 +31,7 @@ class SocialInfo extends StatelessWidget {
               ),
               IconButton(
                 onPressed: () async {
-                  var url = Uri.https('github.com', '/Boahen123');
+                  var url = Uri.https('github.com', '/StormGear');
                   if (await canLaunchUrl(url)) {
                     await launchUrl(url);
                   }
